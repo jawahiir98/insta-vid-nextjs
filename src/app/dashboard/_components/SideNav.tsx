450e8b9c8bc3a6296e500cd6f73e4f958ce02b8c
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-16 16:04:18 +0600

    side nav

---
+++ src/app/dashboard/_components/SideNav.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { CircleUser, PanelsTopLeft, PlusCircleIcon, ShieldPlusIcon } from 'lucide-react'
+
+const menuOptions = [
+    { id: 1, name: 'Dashboard', path: '/dashboard', icon: PanelsTopLeft },
+    { id: 2, name: 'Create New', path: '/create-new', icon: PlusCircleIcon },
+    { id: 3, name: 'Upgrade', path: '/upgrade', icon: ShieldPlusIcon },
+    { id: 4, name: 'Account', path: '/account', icon: CircleUser },
+]
+
+export const SideNav = () => {
+    const pathname = usePathname()
+
+    return (
+        <div className="w-64 h-screen shadow-md p-5">
+            <div>
+                {menuOptions.map((item) => {
+                    const isActive = pathname === item.path
+
+                    return (
+                        <Link href={item.path} key={item.id}>
+                            <div
+                                className={`flex items-center my-[4px] gap-3 p-3 rounded cursor-pointer transition-all duration-300 ease-in-out hover:bg-primary hover:text-primary-foreground hover:scale-105 ${
+                                    isActive ? 'bg-primary text-primary-foreground scale-105' : ''
+                                }`}
+                            >
+                                <item.icon />
+                                <h2>{item.name}</h2>
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import {Header} from "./_components/Header";
+import {Header} from "@/app/dashboard/_components/Header";
+import {SideNav} from "@/app/dashboard/_components/SideNav";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="">
-      <div className="">
-        <Header />
+    <div>
+      <div className="hidden  md:block h-screen bg-white fixed mt-[70px] w-64">
+        <SideNav />
       </div>
-      <div className="">{children}</div>
+      <div>
+        <Header />
+        <div className={'md:ml-64'}>
+            {children}
+        </div>
+      </div>
     </div>
   );
 }
