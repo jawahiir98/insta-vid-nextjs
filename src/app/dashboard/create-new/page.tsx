0b57c9d2cf9e994ee763d7096b2bce8bf29e60e5
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-17 14:46:33 +0600

    Select duration form UI

--- src/app/dashboard/create-new/page.tsx
+++ src/app/dashboard/create-new/page.tsx
@@ -1,15 +1,18 @@
 "use client"
 
+import {Button} from '@/components/ui/button'
 import {SelectTopic} from "@/app/dashboard/create-new/_components/SelectTopic";
 import {SelectStyle} from "@/app/dashboard/create-new/_components/SelectStyle";
+import {SelectDuration} from "@/app/dashboard/create-new/_components/SelectDuration";
 import {useState} from "react";
 
 function CreateNew() {
 
         const [formData, setFormData] = useState([])
         const onHandleInputChange = (fieldName: string, fieldValue:string)=> {
-            console.log(fieldName, fieldValue)
+            setFormData(prev => ({...prev, [fieldName]: fieldValue}))
         }
+        console.log(formData)
         return(
             <div className={'md:px-20'}>
                 <h2 className="font-bold text-3xl text-center">
@@ -21,7 +24,9 @@ function CreateNew() {
                     {/* Select style */}
                     <SelectStyle onUserSelect={onHandleInputChange}/>
                     {/* Duration */}
+                    <SelectDuration onUserSelect={onHandleInputChange}/>
                     {/* Create Button */}
+                    <Button className={'w-full mt-8'}>Create Short Video </Button>
                 </div>
             </div>
         )
