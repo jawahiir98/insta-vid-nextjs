cab7e28e074d0bb5039aefda6e8800fecb54d167
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-21 23:09:39 +0600

    Remotion configed

---
+++ remotion/Root.tsx
@@ -0,0 +1,18 @@
+
+import {Composition} from 'remotion';
+import {RemotionVideo} from '@/app/dashboard/_components/RemotionVideo'
+
+export const RemotionRoot = () => {
+    return (
+        <>
+            <Composition
+                id="Empty"
+                component={RemotionVideo}
+                durationInFrames={60}
+                fps={30}
+                width={1280}
+                height={720}
+            />
+        </>
+    )
+}
