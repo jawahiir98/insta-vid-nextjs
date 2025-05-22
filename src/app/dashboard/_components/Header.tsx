d3c1f6a93b355a983082ef0f884d4bea60797ba1
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-23 00:55:08 +0600

    Add user context and credits to header

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import {Button} from '@/components/ui/button'
 import {UserButton} from "@clerk/nextjs";
-import React from "react";
+import React, {useContext} from "react";
+import {Coins} from 'lucide-react'
+import {UserDetailContext} from "@/app/_context/UserDetailContext";
 
 export const Header = () => {
-  return (
+    const {userDetail} = useContext(UserDetailContext);
+
+    return (
       <div className={'flex justify-between p-4 shadow-sm'}>
         <div className="flex items-center gap-3">
           <Image alt={'logo'} src={'/logo.svg'} width={30} height={30}/>
@@ -14,7 +18,18 @@ export const Header = () => {
         </div>
         <div className="flex items-center gap-3">
           <UserButton/>
-          <Button>Dashboard</Button>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <defs>
+                    <linearGradient id="iconGradient" x1="100%" y1="0%" x2="0%" y2="0%">
+                        <stop offset="0%" stopColor="#3B82F6" /> {/* blue-600 */}
+                        <stop offset="100%" stopColor="#6B21A8" /> {/* purple-700 */}
+                    </linearGradient>
+                </defs>
+                <Coins size={24} fill="url(#iconGradient)" />
+            </svg>
+            <span className={'flex items-center gap-2 font-medium  bg-gradient-to-r from-purple-700 to-blue-600 bg-clip-text text-transparent'}>
+              {userDetail?.credits} Credits
+            </span>
         </div>
       </div>
   )
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,26 +1,44 @@
 "use client"
 
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import {Header} from "@/app/dashboard/_components/Header";
 import {SideNav} from "@/app/dashboard/_components/SideNav";
 import {VideoDataContext} from "@/app/_context/videoDataContext"
+import { UserDetailContext } from "@/app/_context/UserDetailContext";
+import { db } from "@/db/drizzle";
+import { Users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import {useUser} from "@clerk/nextjs";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const [videoData, setVideoData] = useState()
+  const [videoData, setVideoData] = useState();
+  const [userDetail, setUserDetail] = useState();
+  const {user} = useUser();
+  const getUserDetail = async () => {
+      const result = await db.select().from(Users).where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+      setUserDetail(result[0]);
+  }
+
+    useEffect(() => {
+        getUserDetail();
+    }, [user]);
+
   return (
-      <VideoDataContext.Provider value={{videoData,setVideoData}}>
-          <div>
-              <div className="hidden  md:block h-screen bg-white fixed mt-[70px] w-64">
-                  <SideNav/>
-              </div>
+      <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
+          <VideoDataContext.Provider value={{videoData,setVideoData}}>
               <div>
-                  <Header/>
-                  <div className={'md:ml-64 p-10'}>
-                      {children}
+                  <div className="hidden  md:block h-screen bg-white fixed mt-[70px] w-64">
+                      <SideNav/>
+                  </div>
+                  <div>
+                      <Header/>
+                      <div className={'md:ml-64 p-10'}>
+                          {children}
+                      </div>
                   </div>
               </div>
-          </div>
-      </VideoDataContext.Provider>
+          </VideoDataContext.Provider>
+      </UserDetailContext.Provider>
   );
 }
 
