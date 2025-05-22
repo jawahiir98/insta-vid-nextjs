7d2ceb1cc1d8b4118335f9436367565aea8aa52c
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-23 00:33:53 +0600

    user dashboard with created videos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,62 @@
 "use client"
+import { Button } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import {EmptyState} from './_components/EmptyState';
+import Link from 'next/link';
+import {VideoList} from './_components/VideoList';
+import { db } from '@/db/drizzle';
+import { VideoData } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { useUser } from '@clerk/nextjs';
 
-import {useState} from "react";
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
-import {EmptyState} from "@/app/dashboard/_components/EmptyState";
+const Dashboard = () => {
+    const [videoList, setVideoList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const {user} = useUser();
 
-function Dashboard() {
-    const [videoList, setVideoList] = useState([])
-  return (
-    <div>
-      <div className="flex items-center justify-between">
-          <h2 className={'text-2xl font-semibold'}>Dashboard</h2>
-          <Link href='/dashboard/create-new'>
-              <Button>+ Create New</Button>
-          </Link>
-      </div>
-        {videoList?.length === 0 && (
-            <EmptyState />
-        )}
-    </div>
-  );
+    useEffect(()=>{
+        getVideoList();
+    }, [user])
+
+    // Used to get User Videos
+    const getVideoList = async( )=> {
+        setLoading(true);
+        const result = await db.select().from(VideoData)
+            .where(eq(VideoData.createdBy, user?.primaryEmailAddress?.emailAddress));
+        setVideoList(result);
+        setLoading(false);
+    }
+
+    return (
+        <div>
+            <div className=' flex items-center justify-between'>
+                <h2 className='font-bold text-2xl text-primary'>Dashboard</h2>
+                <Link href={"/dashboard/create-new"}>
+                    <Button className='flex items-center p-2 shadow-md'>
+                        <PlusCircle/> Create New
+                    </Button>
+                </Link>
+
+            </div>
+
+            {
+                loading &&
+                <div className='mt-52 flex justify-center items-center text-gray-600 text-2xl font-bold'>
+                    Loading...
+                </div>
+            }
+
+            {!loading && videoList?.length == 0 &&
+                <div>
+                    <EmptyState/>
+                </div>
+            }
+
+            {/* List Of Videos */}
+            <VideoList videoList={videoList} />
+        </div>
+    )
 }
 
-export default Dashboard;
+export default Dashboard
