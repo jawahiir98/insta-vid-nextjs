dce26d84f86cc496a0bdcfb180136e7bfcc6728a
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-23 01:42:01 +0600

    package lock updated

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   env: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_DATABASE_URL: process.env.DATABASE_URL,
   },
 };
 
