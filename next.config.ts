7fe96e4826321d024e8d10407be22446f2711b58
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-23 15:48:49 +0600

    not-found page missing

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
+  experimental: {
+    ppr: true,
+  },
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+    // Instead, use `not-found.ts` as a workaround
+    // "ts" is required to resolve `not-found.ts`
+    "ts"
+  ],
 };
 
 export default nextConfig;
