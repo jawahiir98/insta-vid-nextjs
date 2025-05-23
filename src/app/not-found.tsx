109ddea5c365cc614b8f604f4a529c22fd86184a
Jawahiir Nabhan <jawahiirboss@gmail.com>
2025-05-23 15:28:57 +0600

    not-found page missing

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div style={{ padding: 20, textAlign: 'center' }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you requested does not exist.</p>
+        </div>
+    )
+}
